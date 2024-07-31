7b86192058aa3e98a46cea60778bd5f7e5998169
오병준 <mail@luftaquila.io>
2024-07-31 14:40:08 +0900

    add: usb file list and data dump impl

--- device/firmware/Core/Inc/energymeter_usb.h
+++ device/firmware/Core/Inc/energymeter_usb.h
@@ -4,14 +4,11 @@
 #include <stdint.h>
 
 /******************************************************************************
- * USB commands
+ * USB command / response
  *****************************************************************************/
-#define MAX_LEN_CMD  10
-#define MAX_LEN_RESP 10
+#define MAX_LEN_CMD  15
+#define MAX_LEN_RESP 15
 
-/******************************************************************************
- * USB command / response macro
- *****************************************************************************/
 #define USB_Command(CMD) \
   (strncmp((const char *)UserRxBufferFS, cmd[CMD], strlen(cmd[CMD])) == 0)
 
@@ -24,8 +21,8 @@
 void mode_usb(void);
 
 void usb_set_rtc(uint8_t *buf);
-void usb_load_list(uint8_t *buf);
-void usb_load_all(uint8_t *buf);
+void usb_load_list(void);
+void usb_load_all(void);
 void usb_load_one(uint8_t *buf);
 
 #endif /* CORE_INC_ENERGYMETER_USB_H */
--- device/firmware/Core/Src/energymeter_usb.c
+++ device/firmware/Core/Src/energymeter_usb.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdint.h>
 #include <string.h>
 
@@ -13,6 +14,7 @@
 /* USB CDC receive flag and buffer */
 uint32_t usb_flag = FALSE;
 extern uint8_t UserRxBufferFS[];
+extern uint8_t UserTxBufferFS[];
 
 /* USB CDC command from the host system */
 typedef enum {
@@ -25,23 +27,37 @@ typedef enum {
 } usb_cmd_type_t;
 
 const char cmd[CMD_COUNT][MAX_LEN_CMD + 1] = {
-  "SET-ID",
-  "SET-RTC",
-  "LOAD-LIST",
-  "LOAD-ALL",
-  "LOAD-ONE",
+  "$SET-ID",
+  "$SET-RTC",
+  "$LOAD-LIST",
+  "$LOAD-ALL",
+  "$LOAD-ONE",
 };
 
 /* USB CDC response from here */
 typedef enum {
   RESP_LIST_START,
   RESP_LIST_END,
+  RESP_LOAD_ALL_START,
+  RESP_LOAD_ALL_END,
+  RESP_FILE_ENTRY,
+  RESP_FILE_START,
+  RESP_FILE_END,
+  RESP_OK,
+  RESP_ERROR,
   RESP_COUNT,
 } usb_response_type_t;
 
 const uint8_t resp[RESP_COUNT][MAX_LEN_RESP + 1] = {
-  "LIST-START",
-  "LIST-END",
+  "$LIST-START",
+  "$LIST-END",
+  "$LOAD-ALL-START",
+  "$LOAD-ALL-END",
+  "$FILE-ENTRY",
+  "$FILE-START",
+  "$FILE-END",
+  "$OK",
+  "$ERROR",
 };
 
 /******************************************************************************
@@ -62,8 +78,8 @@ void mode_usb(void) {
   uint32_t tick = HAL_GetTick();
 
   // we don't need to be super fast here.
-  // let's just compare strings and busy wait led blink
-  while (TRUE) {
+  // let's just play with strings and busy wait led blink
+  while (1) {
     static uint32_t cur = 0;
     cur = HAL_GetTick();
 
@@ -81,19 +97,19 @@ void mode_usb(void) {
     }
 
     else if (USB_Command(CMD_SET_RTC)) {
-      usb_set_rtc(UserRxBufferFS + strlen(cmd[CMD_SET_RTC]));
+      usb_set_rtc(UserRxBufferFS + strlen(cmd[CMD_SET_RTC]) + 1);
     }
 
     else if (USB_Command(CMD_LOAD_LIST)) {
-      usb_load_list(UserRxBufferFS + strlen(cmd[CMD_LOAD_LIST]));
+      usb_load_list();
     }
 
     else if (USB_Command(CMD_LOAD_ALL)) {
-      usb_load_all(UserRxBufferFS + strlen(cmd[CMD_LOAD_ALL]));
+      usb_load_all();
     }
 
     else if (USB_Command(CMD_LOAD_ONE)) {
-      usb_load_one(UserRxBufferFS + strlen(cmd[CMD_LOAD_ONE]));
+      usb_load_one(UserRxBufferFS + strlen(cmd[CMD_LOAD_ONE]) + 1);
     }
   }
 }
@@ -108,11 +124,10 @@ void usb_set_rtc(uint8_t *buf) {
 /******************************************************************************
  * load all saved files list
  *****************************************************************************/
-void usb_load_list(uint8_t *buf) {
+void usb_load_list(void) {
   uint8_t usb_ret;
   USB_Response(RESP_LIST_START);
 
-  FRESULT ret;
   FILINFO fno;
   DIR dir;
 
@@ -121,28 +136,112 @@ void usb_load_list(uint8_t *buf) {
   fno.lfsize = sizeof(filename);
 
   while (TRUE) {
-    ret = f_findfirst(&dir, &fno, "", "*.log");
+    FRESULT ret = f_findfirst(&dir, &fno, "", "*.log");
 
     while (ret == FR_OK && fno.fname[0]) {
-      // TODO
+      sprintf((char *)UserTxBufferFS, "%s %lu %s", resp[RESP_FILE_ENTRY], fno.fsize, fno.lfname);
+      USB_Transmit(UserTxBufferFS, strlen((const char *)UserTxBufferFS));
 
       ret = f_findnext(&dir, &fno);
     }
 
     f_closedir(&dir);
   }
+
+  USB_Response(RESP_LIST_END);
 }
 
 /******************************************************************************
  * load all saved files
  *****************************************************************************/
-void usb_load_all(uint8_t *buf) {
-  // TODO
+void usb_load_all(void) {
+  uint8_t usb_ret;
+  USB_Response(RESP_LOAD_ALL_START);
+
+  FILINFO fno;
+  DIR dir;
+
+  char filename[_MAX_LFN];
+  fno.lfname = filename;
+  fno.lfsize = sizeof(filename);
+
+  while (TRUE) {
+    FRESULT ret = f_findfirst(&dir, &fno, "", "*.log");
+
+    while (ret == FR_OK && fno.fname[0]) {
+      sprintf((char *)UserTxBufferFS, "%s %lu %s", resp[RESP_FILE_ENTRY], fno.fsize, fno.lfname);
+      USB_Transmit(UserTxBufferFS, strlen((const char *)UserTxBufferFS));
+
+      FIL fp;
+
+      if (f_open(&fp, fno.lfname, FA_READ) != FR_OK) {
+        USB_Response(RESP_ERROR);
+        return;
+      };
+
+      uint32_t total_read = 0;
+
+      while (total_read < f_size(&fp)) {
+        uint32_t read;
+
+        if (f_read(&fp, UserTxBufferFS, APP_TX_DATA_SIZE, (UINT *)&read) != FR_OK) {
+          USB_Response(RESP_ERROR);
+          USB_Response(RESP_FILE_END);
+          return;
+        }
+
+        total_read += read;
+        USB_Transmit(UserTxBufferFS, read);
+      }
+
+      USB_Response(RESP_FILE_END);
+
+      ret = f_findnext(&dir, &fno);
+    }
+
+    f_closedir(&dir);
+  }
+
+  USB_Response(RESP_LOAD_ALL_END);
 }
 
 /******************************************************************************
  * load one file by its name
  *****************************************************************************/
 void usb_load_one(uint8_t *buf) {
-  // TODO
+  uint8_t usb_ret;
+
+  uint8_t *end = (uint8_t *)strchr((const char *)buf, ' ');
+  *end = '\0';
+
+  int namelen = atoi((const char *)buf);
+  buf = end + 1;
+  *(buf + namelen) = '\0';
+
+  FIL fp;
+
+  if (f_open(&fp, (const char *)buf, FA_READ) != FR_OK) {
+    USB_Response(RESP_ERROR);
+    return;
+  };
+
+  sprintf((char *)UserTxBufferFS, "%s %lu", resp[RESP_FILE_START], f_size(&fp));
+  USB_Transmit(UserTxBufferFS, strlen((const char *)UserTxBufferFS));
+
+  uint32_t total_read = 0;
+
+  while (total_read < f_size(&fp)) {
+    uint32_t read;
+
+    if (f_read(&fp, UserTxBufferFS, APP_TX_DATA_SIZE, (UINT *)&read) != FR_OK) {
+      USB_Response(RESP_ERROR);
+      USB_Response(RESP_FILE_END);
+      return;
+    }
+
+    total_read += read;
+    USB_Transmit(UserTxBufferFS, read);
+  }
+
+  USB_Response(RESP_FILE_END);
 }
