381ee96241be1477daca6b1bf9389b795c5fa1e1
오병준 <mail@luftaquila.io>
2024-08-06 23:21:56 +0900

    improve: always trigger bus enumeration on the host

--- device/firmware/Core/Src/usb.c
+++ device/firmware/Core/Src/usb.c
@@ -32,7 +32,19 @@ void MX_USB_PCD_Init(void)
 {
 
   /* USER CODE BEGIN USB_Init 0 */
+  /* pull down PA12 USB_DP for 5ms to trigger bus enumeration on the host system */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12, GPIO_PIN_RESET);
+
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  HAL_Delay(5);
   /* USER CODE END USB_Init 0 */
 
   /* USER CODE BEGIN USB_Init 1 */
